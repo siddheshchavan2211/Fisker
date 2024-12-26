69b88319a7822fe3ba45dfe851d3347ff4069114
Siddhesh Chavan <siddheshchavan02@gmail.com>
2024-12-26 12:32:54 +0000

    config added

--- fisker/components/Configurator.jsx
+++ fisker/components/Configurator.jsx
@@ -1,95 +1,88 @@
 import Image from "next/image";
 import React from "react";
+import { exteriorSwatch, interiorSwatch, wheelSwatch } from "@/lib/swach";
 
-const Configurator = () => {
+function Configurator() {
   return (
-    <div>
-      <h1 className="mb-4 text-3xl font-bold text-blue-900">Fisker Ocean</h1>
-      <p className="mb-4">
-        Configure your very own, with the options to choose from 6 exterior, 2
-        interior and 3 wheels options.
+    <>
+      <h1 className="mb-4 text-4xl font-bold">Fisker Ocean</h1>
+      <p>
+        Configure your very own, with the option to choose from 6 exterior, 2
+        interior and 3 wheel options.
       </p>
-      <h4 className="font-bold uppercase mb-2"> Exterior Color</h4>
-      <div className="flex gap-1">
-          {new Array(6).fill(0).map(
-            (
-              _,
-              i, // 6 colors
-            ) => (
-              <button
-                key={i}
-                className={`${i === 0 && "ring"} rounded-full ring-blue-300 duration-100 hover:scale-110`}
-                onClick={() => console.log(i + 1)}
-              >
-                <Image
-                  src="/sun_soaked.webp"
-                  alt="Color"
-                  width={50}
-                  height={50}
-                />
-              </button>
-            ),
-          )}
+
+      {/* Exterior Color */}
+      <div className="my-4">
+        <h3 className="mb-2 text-lg font-bold">Exterior Color</h3>
+
+        <div className="flex flex-wrap gap-2">
+          {exteriorSwatch.map((swatch) => (
+            <button
+              key={swatch.id}
+              className={`${swatch.id === 1 && "ring"} rounded-full ring-blue-300 duration-100 hover:scale-105`}
+              onClick={() => console.log(i + 1)}
+            >
+              <Image
+                src={swatch.src}
+                alt="Color"
+                width={0}
+                height={0}
+                sizes="100%"
+                className="w-11"
+              />
+            </button>
+          ))}
         </div>
+      </div>
 
       {/* Wheel Color */}
       <div className="my-4">
         <h3 className="mb-2 text-lg font-bold">Wheel Options</h3>
 
         <div className="flex gap-2">
-          {new Array(3).fill(0).map(
-            (
-              _,
-              i, // 6 colors
-            ) => (
-              <button
-                key={i}
-                className={`${i === 0 && "ring"} rounded-full ring-blue-300 duration-100 hover:scale-105`}
-                onClick={() => console.log(i + 1)}
-              >
-                <Image
-                  src="/sun_soaked.webp"
-                  alt="Color"
-                  width={0}
-                  height={0}
-                  sizes="100%"
-                  className="w-14"
-                />
-              </button>
-            ),
-          )}
+          {interiorSwatch.map((swatch) => (
+            <button
+              key={swatch.id}
+              className={`${swatch.id === 0 && "ring"} rounded-full ring-blue-300 duration-100 hover:scale-105`}
+              onClick={() => console.log(swatch.id + 1)}
+            >
+              <Image
+                src={swatch.src}
+                alt="Color"
+                width={0}
+                height={0}
+                sizes="100%"
+                className="w-14"
+              />
+            </button>
+          ))}
         </div>
       </div>
-        {/* Interior Color */}
-        <div className="my-4">
+
+      {/* Interior Color */}
+      <div className="my-4">
         <h3 className="mb-2 text-lg font-bold">Interior Color</h3>
 
         <div className="flex gap-2">
-          {new Array(2).fill(0).map(
-            (
-              _,
-              i, // 6 colors
-            ) => (
-              <button
-                key={i}
-                className={`${i === 0 && "ring"} rounded-full ring-blue-300 duration-100 hover:scale-105`}
-                onClick={() => console.log(i + 1)}
-              >
-                <Image
-                  src="/sun_soaked.webp"
-                  alt="Color"
-                  width={0}
-                  height={0}
-                  sizes="100%"
-                  className="w-14"
-                />
-              </button>
-            ),
-          )}
+          {wheelSwatch.map((swatch) => (
+            <button
+              key={swatch.id}
+              className={`${swatch.id === 0 && "ring"} ring-blue-300 duration-100 hover:scale-105`}
+              onClick={() => console.log(swatch.id + 1)}
+            >
+              <Image
+                src={swatch.src}
+                alt="Color"
+                width={0}
+                height={0}
+                sizes="100%"
+                className="w-14"
+              />
+            </button>
+          ))}
         </div>
       </div>
-    </div>
+    </>
   );
-};
-
+}
 export default Configurator;
